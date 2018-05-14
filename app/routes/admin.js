ff5347d959929c342b64bab3e7de60090300fda7
tiagoalucard <tiagoalucard@gmail.com>
2018-05-14 20:16:19 -0300

    alteracao

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,17 +1,34 @@
 module.exports = function(app) {
 
   app.get('/formulario_inclusao_noticia', function(req, res){
-    res.render('admin/form_add_noticia')
+    res.render('admin/form_add_noticia', {validacao: {}, noticia: {}});
   });
 
   app.post('/noticias/salvar', function(req, res){
     var noticia = req.body;
 
-    var connection = app.config.dbConnection();
-    var noticiasModel = app.app.models.noticiasModel;
+    console.log(noticia);
+    req.assert('titulo','Título é obrigatório').notEmpty();
+    req.assert('resumo','Resumo é obrigatório').notEmpty();
+    req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+    req.assert('autor','Autor é obrigatório').notEmpty();
+    req.assert('data_noticia','Data é obrigatório').notEmpty();
+    req.assert('noticia','Noticia é obrigatório').notEmpty();
 
-    noticiasModel.salvarNoticias(noticia, connection, function(){
-      res.redirect('/noticias/')    
+    var erros = req.validationErrors();
+
+    console.log(erros);
+
+    if(erros){
+      res.render('admin/form_add_noticia', {validacao : erros, noticia: noticia});
+      return;
+    }
+
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    noticiasModel.salvarNoticias(noticia, function(error, result){
+      res.redirect('/noticias/');
     });  
   });
 };
